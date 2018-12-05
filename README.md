e1ac6d08e91022c22668a14ffc9078cdb1fb8a12
lucianocarvalho <luciano_722@hotmail.com>
2018-12-04 22:42:49 -0200

    Users endpoints

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# FaeduApp
+# API FAEDU
+A API foi construia seguindos os padrões REST e usando Node.js/Express.
+A autenticação é feita através do JWT, que deve ser gerado no login e passado nas requisições posteriores.
+
+### Endpoints
+Abaixo estão todos os endpoints que são escutados pela a API.
+
+##### 1. Login
+* **POST /api/login**: Realiza o login e retorna um token JWT.
+ 
+##### 2. Usuários
+* **POST /api/users**: Cadastra um novo usuário.
+* **GET /api/users/:id**: Retorna os dados do usuário **(protegida)**.
+* **PUT /api/users/:id**: Atualiza os dados do usuário **(protegida)**.
+* **DELETE /api/users/:id**: Deleta o usuário do banco **(protegida)**.
--- api/routes/users.js
+++ api/routes/users.js
@@ -21,6 +21,67 @@ module.exports = ( app ) => {
                 message: 'User registered successfully!'
             });
         });
+    });
+
+    // Return an user by id.
+    app.get('/api/users/:id', VerifyToken, ( req, res, next ) => {
+
+        Users.findOne({
+            _id: req.params.id
+        }, ( err, data ) => {
+
+            if( err ) {
+                const error = new Error( err.message );
+                error.httpStatusCode = 400;
+                return next( error );
+            }
             
+            if( data ) {
+                return res.status( 200 ).json({
+                    status: true,
+                    user: data
+                });
+            }
+        });        
+    });
+
+    // Update an user.
+    app.put('/api/users/:id', VerifyToken, ( req, res, next ) => {
+
+        Users.findOneAndUpdate({
+            _id: req.params.id
+        }, req.body, ( err ) => {
+
+            if( err ) {
+                const error = new Error( err.message );
+                error.httpStatusCode = 400;
+                return next( error );
+            }
+
+            return res.status( 200 ).json({
+                status: true,
+                message: "User successfully updated!"
+            });
+        });
+    });
+
+    // Delete an user.
+    app.delete('/api/users/:id', VerifyToken, ( req, res, next ) => {
+
+        Users.deleteOne({
+            _id: req.params.id
+        }, ( err ) => {
+
+            if( err ) {
+                const error = new Error( err.message );
+                error.httpStatusCode = 400;
+                return next( error );
+            }
+
+            return res.status( 200 ).json({
+                status: true,
+                message: "User successfully removed!"
+            });
+        });
     });
 }
